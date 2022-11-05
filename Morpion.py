fad76cf092eb2407b483973d28483da2149aee9e
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-05 14:46:29 +0100

    Le fichier morpion, debut

---
+++ Morpion.py
@@ -0,0 +1,35 @@
+"""
+Mohamed HABBAAINA, le 20 oct 2022
+Écrivez un programme morpion.py, qui permet à l’utilisateur de faire une partie à 2
+joueurs du célèbre jeu le morpion dans le terminal.
+Le programme devra dans un premier temps demander si on souhaite jouer ou voir les
+scores. Puis, il demandera aux deux joueurs de renseigner leur ursername. Il affichera
+ensuite la map uniquement remplie de “-”.
+Le programme demandera au premier joueur, les croix, de rentrer en input une ligne et
+une colonne, où il souhaite jouer.
+Le programme ré affichera la map, avec une croix là où le joueur a choisi.
+C’est au tour des ronds de jouer, et ainsi de suite.
+- - -   - - x
+- - -   - - -
+- - -   - - -
+Une fois la partie finie, le programme devra écrire dans un fichier score.txt les username,
+et leur nombre de victoires. Si le username du gagnant existe déjà, il devra incrémenter
+son nombre de victoires de 1. Sinon, il ajoutera à la fin du fichier son username, avec
+une victoire.
+# """
+# a = input("souhaites tu jouer ou voir les scores? (y/n)")
+# if a == "n":
+#     print("Au revoir")
+# else: 
+#     joeur_1 = input("nom de premier joueur:")
+#     joeur_2 = input("nom de deuxième joueur:")
+#     # tab = [ "-  -  -" , "- - -" , "- - -"]
+    # for i in range(3):
+    #     print()
+    #     for j in range(3):
+    #         print("-", end="")
+    
+
+    # choix = 
+    # for i in range
+    
